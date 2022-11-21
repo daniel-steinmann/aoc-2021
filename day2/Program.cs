95e588354496c6ea5e260b86d46ebecfde9647d3
Daniel Steinmann <daniel.steinmann1.ext@swica.ch>
2022-11-21 07:53:25 +0100

    Add first files.

---
+++ day2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace MyApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = "input.txt";
+            string[] lines = File.ReadAllLines(filePath);
+
+            List<(string,int)> commands = new();
+            foreach (string line in lines){
+                string[] splitted = line.Split();
+                (string, int) single = (splitted[0], int.Parse(splitted[1]));
+                commands.Add(single);
+            }
+
+            commands.ForEach(x => Console.WriteLine("{0}, {1}", x.Item1, x.Item2));
+
+            int aim = 0;
+            int pos = 0;
+            int depth = 0;
+            foreach ((string, int) command in commands){
+                _ = command.Item1 switch
+                {
+                    "forward" => adjustAim(command.Item2, aim, ref pos, ref depth),
+                    "down" => aim += command.Item2,
+                    "up" => aim -= command.Item2,
+                    _ => throw new ArgumentException("Invalid switch"),
+                };
+
+                Console.WriteLine("Pos: {0}, Depth: {1}, Aim: {2}",pos, depth, aim);
+            }
+            Console.WriteLine("Final: Pos: {0}, Depth: {1}, Mult: {2}",pos, depth, pos*depth);
+        }
+
+        static int adjustAim(int x, int aim, ref int pos, ref int depth){
+            pos += x;
+            depth += aim*x;
+            return 1;
+        }
+    }
+}
---
+++ day3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace App
+{
+    class Day3
+    {
+        static void Main(string[] args)
+        {
+            string filePath = "sample_input.txt";
+            string[] lines = File.ReadAllLines(filePath);
+
+            Array.ForEach(lines, x => Console.WriteLine(x));
+
+            List<int> records = lines.Select(x => Int16.Parse(x));
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("{0}", 1 << i);
+
+                foreach (int )
+            }
+        }
+    }
+}
